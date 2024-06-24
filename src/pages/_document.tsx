6fed1746584b3fe3c08a74f52077e9dda39d4f41
ankushKun <ankush4singh@gmail.com>
2024-06-25 01:27:38 +0530

    ugh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>BombAR - Nuke your friends for fun</title>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossOrigin="" />
+
+        {/* <!-- Make sure you put this AFTER Leaflet's CSS --> */}
+        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+          crossOrigin=""></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
